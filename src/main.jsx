316e56b20190c8eda0d94a9c15d1f2d22afd2aee
AbdullahiTama <99316338+AbdullahiTama@users.noreply.github.com>
2026-06-30 08:44:37 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { AuthProvider } from './lib/AuthContext.jsx'
 import App from './App.jsx'
 import BusinessProfile from './BusinessProfile.jsx'
 import Login from './Login.jsx'
+import Profile from './Profile.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="/" element={<App />} />
           <Route path="/business/:id" element={<BusinessProfile />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
