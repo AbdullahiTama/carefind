49e8b364a3e03b5f579e57a38358e2e6dbae99bb
AbdullahiTama <99316338+AbdullahiTama@users.noreply.github.com>
2026-06-30 13:30:00 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './lib/AuthContext.jsx'
-import App from './App.jsx'
+import Feed from './Feed.jsx'
+import Search from './Search.jsx'
 import BusinessProfile from './BusinessProfile.jsx'
 import Login from './Login.jsx'
 import Profile from './Profile.jsx'
-import Feed from './Feed.jsx'
 import PublicProfile from './PublicProfile.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -14,11 +14,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<Feed />} />
+          <Route path="/search" element={<Search />} />
           <Route path="/business/:id" element={<BusinessProfile />} />
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/feed" element={<Feed />} />
           <Route path="/u/:id" element={<PublicProfile />} />
         </Routes>
       </BrowserRouter>
